d0e34d0acbaf3c6792b9d5b11c2933026957f4ed
DriptaSenapati <driptasenapati97@gmail.com>
2025-04-19 21:57:56 +0530

    feedback page added

--- src/app/(frontend)/give-review/page.tsx
+++ src/app/(frontend)/give-review/page.tsx
@@ -1,5 +1,11 @@
 import GiveReviewBody from '@/components/frontend/give-review/GiveReviewBody'
+import { Metadata } from 'next'
 import React from 'react'
+
+export const metadata: Metadata = {
+  title: 'Feedback Form | Dripta Senapati',
+  description: 'Provide feedback for Dripta Senapati',
+}
 
 const GiveReview = () => {
   return <GiveReviewBody />
