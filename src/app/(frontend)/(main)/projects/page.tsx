89f6bd2234645c702b6f87f05c0dd2c00bd2009f
DriptaSenapati <driptasenapati97@gmail.com>
2025-05-09 23:37:41 +0530

    project route SSR fixed

--- src/app/(frontend)/(main)/projects/page.tsx
+++ src/app/(frontend)/(main)/projects/page.tsx
@@ -1,13 +1,31 @@
+import { getProjectList } from '@/actions/projectActions'
 import ProjectRoute from '@/components/frontend/projects/ProjectRoute'
+import { project_fetch_key } from '@/lib/swrKeys'
 import { Metadata } from 'next'
 import React from 'react'
+import { SWRConfig } from 'swr'
 
 export const metadata: Metadata = {
   title: 'Projects | Dripta Senapati',
 }
 
-const Projects = () => {
-  return <ProjectRoute />
+const getProjectDetails = async () => {
+  const projects = await getProjectList()
+
+  return {
+    fallback: {
+      [project_fetch_key]: projects,
+    },
+  }
+}
+
+const Projects = async () => {
+  const { fallback } = await getProjectDetails()
+  return (
+    <SWRConfig value={{ fallback }}>
+      <ProjectRoute />
+    </SWRConfig>
+  )
 }
 
 export default Projects
